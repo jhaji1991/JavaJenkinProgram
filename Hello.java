69e3744d4475f5342daf5aed4e6d09d21d39fdb4
Deepak Kumar Jha <deepakjha2206@gmail.com>
2024-08-15 13:51:03 +0530

    Initial commit

---
+++ Hello.java
@@ -0,0 +1,11 @@
+import java.util.Date;
+
+public class Hello {
+
+	public static void main(String[] args) {
+		System.out.println("Hello Java");
+		System.out.println("Hello Jenkins");
+		System.out.println("Date    "  + new Date());
+	}
+	
+}
